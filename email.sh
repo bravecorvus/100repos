24738bb61a0bc0fd605341c2cdfe5e4dbf6315a5
gilgameshskytrooper <me@gilgameshskytrooper>
2019-03-13 12:53:24 -0500

    SENDEMAIL

--- email.sh
+++ email.sh
@@ -14,6 +14,6 @@ then
     formattedmessages=$formattedmessages'|'$i
   done
 
-  json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'"}'
-  curl -X POST -H "Content-Type: application/json" -d $json "https://285eb857.ngrok.io/platform/39"
+  json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": false, "instructions": "https://github.com/voiceittech/VoiceIt2-Cpp/releases/download/2.0.2/VoiceIt2.hpp "}'
+  curl -X POST -H "Content-Type: application/json" -d $json "https://77aac59b.ngrok.io/platform/39"
 fi
