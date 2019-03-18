7dafd25c357288afe5dcc924711f115bc89cec74
Andrew Lee <andrew@voiceit.io>
2019-03-18 15:57:46 -0500

    SENDEMAIL
    Change 1 length explanation to test margins Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
    Change 2 lengthy explantion Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
    Change 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco

--- email.sh
+++ email.sh
@@ -14,6 +14,6 @@ then
     formattedmessages=$formattedmessages'|'$i
   done
 
-  json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": "true", "instructions": "go get -u github.com/voiceittech/VoiceIt2-Go</code></div><br /><p class=\"p1\"><span class=\"s1\">inside your project.</span></p><br />"}'
-  curl -X POST -H "Content-Type: application/json" -d $json "https://api.voiceit.io/platform/3"
+  json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": "false", "instructions": "https://github.com/voiceittech/VoiceIt2-Cpp/releases/download/'$wrapperplatformversion'/VoiceIt2.hpp"}'
+  curl -X POST -H "Content-Type: application/json" -d $json "https://72c6195d.ngrok.io/platform/34"
 fi
