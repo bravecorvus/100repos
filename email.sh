fd434f306905343be00068807a084a2e1fed197a
Andrew Lee <me@gilgameshskytrooper.io>
2019-03-18 12:04:12 -0500

    SENDEMAIL
    Change 1 length explanation to test margins Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
    Change 2 lengthy explantion Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
    Change 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco

--- email.sh
+++ email.sh
@@ -14,6 +14,6 @@ then
     formattedmessages=$formattedmessages'|'$i
   done
 
-  json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": "true", "instructions": "go get github.com/voiceittech/VoiceIt2-Go@2.0.0</code></div><br /><p class="p1"><span class="s1">(or if using Go version < 1.11 and or a <code>GOPATH</code> based installation of our library, run the command: )<span></p> <br /><div style=\"width: 80%; margin: auto; text-align: center;\"><code>go get -u github.com/voiceittech/VoiceIt2-Go</code></div><br />"}'
+  json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": "true", "instructions": "go get github.com/voiceittech/VoiceIt2-Go@2.0.0</code></div><br /><p class="p1"><span class="s1">(or if using Go version < 1.11 and/or a <code>GOPATH</code> based installation of our library, run the command: )<span></p><br /><div style="width: 80%; margin: auto; text-align: center;"><code>go get -u github.com/voiceittech/VoiceIt2-Go</code></div><br />"}'
   curl -X POST -H "Content-Type: application/json" -d $json "https://api.voiceit.io/platform/3"
 fi
